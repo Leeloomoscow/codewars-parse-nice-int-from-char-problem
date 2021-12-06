cb309765a43296f920559b3896bb07b5b72c28c7
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-12-06 21:05:01 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,24 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void test1() {
+        assertEquals(5, CharProblem.howOld("5 years old"));
+    }
+    @Test
+    public void test2() {
+        assertEquals(9, CharProblem.howOld("9 years old"));
+    }
+    @Test
+    public void test3() {
+        assertEquals(1, CharProblem.howOld("1 year old"));
+    }
 
+    private static class CharProblem {
+        public static int howOld(String herOld) {
+            return (int) herOld.charAt(0)-'0';
+        }
     }
 }
